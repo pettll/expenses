172a2f1600c0b30d100830b0ad0fe76772d3ca15
Peter Muller <p.smuller@outlook.com>
2026-03-17 20:20:05 +0000

    Initial Commit

---
+++ expensesTests/expensesTests.swift
@@ -0,0 +1,17 @@
+//
+//  expensesTests.swift
+//  expensesTests
+//
+//  Created by Peterson Segatto Muller on 17/03/2026.
+//
+
+import Testing
+@testable import expenses
+
+struct expensesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
